6f8f669b5c17e70b66d8b69e2b2d65263ce94855
Georgi <georgi.parapanov@gmail.com>
2015-11-02 13:38:37 +0000

    Initial commit

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    echo "Hello, world!";
+    echo "Hello," . " " . "world" . "!";
+    $donut="SARUMAN";
+    echo $donut." IS COMING";
+    ?>
+</p>
+</body>
+</html>
